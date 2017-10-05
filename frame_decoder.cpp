fddb5303d7605a6e2f0373df4664ee7fb908f679
McFlip <gradydenton@yahoo.com>
2017-10-05 01:56:11 -0400

    just getting started

---
+++ frame_decoder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <bitset>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    bitset<8> octet;
+    vector<bool> bit_v;
+    vector<char> ch_v;
+}
