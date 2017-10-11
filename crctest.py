cf6d02725539b202b16f21c4f9145720c16ef5e5
McFlip <gradydenton@yahoo.com>
2017-10-10 23:32:55 -0400

    First draft. Needs debugging.

---
+++ crctest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+def crc_remainder(input_bitstring, polynomial_bitstring, initial_filler):
+	'''
+	Calculates the CRC remainder of a string of bits using a chosen polynomial.
+	initial_filler should be '1' or '0'.
+	'''
+	len_input = len(input_bitstring)
+	#initial_padding = initial_filler  * (len(polynomial_bitstring) - 1)
+	#input_padded_array = list(input_bitstring + initial_padding)
+	input_padded_array = list(input_bitstring + '0001110010110001')
+	#polynomial_bitstring = polynomial_bitstring.lstrip('0')
+	while '1' in input_padded_array[:len_input]:
+		cur_shift = input_padded_array.index('1')
+		for i in range(len(polynomial_bitstring)):
+			if polynomial_bitstring[i] == input_padded_array[cur_shift + i]:
+				input_padded_array[cur_shift + i] = '0'
+			else:
+				input_padded_array[cur_shift + i] = '1'
+	return ''.join(input_padded_array)[len_input:]
+
+myinput = '00110111'
+generator = '10110000100010011'
+filler = '0'
+
+print crc_remainder(myinput, generator, filler)
--- frame_decoder.py
+++ frame_decoder.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-# Grady Denton for proj1 in cnt5505 data comm
+'''
+Grady Denton for proj1 in cnt5505 data comm
+'''
 import argparse
 import os
 from bitstring import Bits, BitArray
@@ -7,31 +9,56 @@ import binascii
 
 # Function definitions
 def decode(frame, num, msg):
-	checksum = frame[-2:]
-	payload = frame[0:-2]
-	if validate(payload, checksum):
-		msg.append(str(payload))
-		print "frame ", num, ", size ", len(payload), ": valid"
-	else:
-		print "frame ", num, ", size ", len(payload), ": invalid"
-	del frame[:]
+  if len(frame) <3:
+    print "frame ", num, ", size ", len(payload), ": invalid"
+  else:
+    checksum = frame[-2:]
+    print "checkbits: ", binascii.hexlify(checksum)
+    payload = frame[0:-2]
+    if validate(payload, Bits(bytes=checksum).bin[2:]):
+      msg.append(str(payload))
+      print "frame ", num, ", size ", len(payload), ": valid"
+    else:
+      print "frame ", num, ", size ", len(payload), ": invalid"
+    del frame[:]
 
 def validate(p, cs):
-	twobit = scramble(p)
-	if cs == calcCheckSum(twobit):
-		return True
-	else:
-		return False
+  twobit = scramble(p)
+  if calcCheckSum(twobit.bin, cs) == 0:
+    return True
+  else:
+    return False
 
 def scramble(ordered):
-	sbits = BitArray()
-	#add for loop cuz arg is bytes
-	orderedBits = Bits(ordered)
-	#do scramble
-	return sbits
+  sbits = BitArray()
+  print "FUBAR: ", ordered
+  for byte in ordered:
+    print "ordered: ", byte
+    b = Bits(uint=byte, length=8)
+    print "ordered: ", b.bin
+    sbits.append(BitArray(bool=b[0]))
+    sbits.append(BitArray(bool=b[4]))
+    sbits.append(BitArray(bool=b[1]))
+    sbits.append(BitArray(bool=b[5]))
+    sbits.append(BitArray(bool=b[2]))
+    sbits.append(BitArray(bool=b[6]))
+    sbits.append(BitArray(bool=b[3]))
+    sbits.append(BitArray(bool=b[7]))
 
-def calcCheckSum(i):
-	return 1
+    print "scramble: ", sbits.bin
+  return sbits
+
+def calcCheckSum(input_bitstring, checksum):
+	len_input = len(input_bitstring)
+	input_padded_array = list(input_bitstring + checksum.zfill(16))
+	while '1' in input_padded_array[:len_input]:
+		cur_shift = input_padded_array.index('1')
+		for i in range(len(polynomial_bitstring)):
+			if polynomial_bitstring[i] == input_padded_array[cur_shift + i]:
+				input_padded_array[cur_shift + i] = '0'
+			else:
+				input_padded_array[cur_shift + i] = '1'
+	return int(''.join(input_padded_array)[len_input:], 2)
 
 # set up arguments
 parser = argparse.ArgumentParser(prog='frame_decoder', description='Parses a dump file and extracts the frames.')
@@ -43,11 +70,11 @@ args = parser.parse_args()
 #infile
 infile = os.path.expanduser(args.infile)
 if not os.path.exists(infile):
-	parser.error('The encoded file does not exist!')
+  parser.error('The encoded file does not exist!')
 if not os.path.isfile(infile):
-	parser.error('The encoded file is not a file!')
+  parser.error('The encoded file is not a file!')
 if not os.access(infile, os.R_OK):
-	parser.error('The encoded file is not readable!')
+  parser.error('The encoded file is not readable!')
 
 #outDir
 #outfile
@@ -67,6 +94,8 @@ frameNum = 0
 data = []
 currentFrame = bytearray()
 byte = bytearray(open(infile, 'rb').read())
+global polynomial_bitstring
+polynomial_bitstring = '10110000100010011'
 
 # TESTING
 #print byte
@@ -75,25 +104,25 @@ byte = bytearray(open(infile, 'rb').read())
 # Process the dump file
 byte_itr = byte.__iter__()
 for byte_val in byte_itr:
-	byte_val = byte_itr.next()
-	#print "current byte is {:x}".format(byte_val)
-	if byte_val != ord('a'):
-		while byte_val != ord('a'):
-			# unstuff; check for escape 'b'
-			if byte_val == ord('b'):
-				byte_val = byte_itr.next()
-				currentFrame.append(byte_val)
-				#print "append byte {:x}".format(byte_val)
-				byte_val = byte_itr.next()
-			else:
-				currentFrame.append(byte_val)
-				#print "append byte {:x}".format(byte_val)
-				byte_val = byte_itr.next()
-		#end of frame - process frame
-		print binascii.hexlify(currentFrame)
-		decode(currentFrame, frameNum, data)
-		#advance to next
-		#byte_val = byte_itr.next()
-		frameNum += 1
+  byte_val = byte_itr.next()
+  #print "current byte is {:x}".format(byte_val)
+  if byte_val != ord('a'):
+    while byte_val != ord('a'):
+      # unstuff; check for escape 'b'
+      if byte_val == ord('b'):
+        byte_val = byte_itr.next()
+        currentFrame.append(byte_val)
+        #print "append byte {:x}".format(byte_val)
+        byte_val = byte_itr.next()
+      else:
+        currentFrame.append(byte_val)
+        #print "append byte {:x}".format(byte_val)
+        byte_val = byte_itr.next()
+    #end of frame - process frame
+    print binascii.hexlify(currentFrame)
+    decode(currentFrame, frameNum, data)
+    #advance to next
+    #byte_val = byte_itr.next()
+    frameNum += 1
 print "processed {} frames".format(frameNum)
 print "".join(data)
